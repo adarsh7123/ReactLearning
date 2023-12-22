a65d5719ed0f4ac8a5a03e2b5e535ab1a8eec083
Verma Adarshkumar Dharmendrakumar <adarshverma127@gmail.com>
2023-12-22 15:56:27 +0530

    React basic 01

---
+++ 01react/src/App.js
@@ -0,0 +1,14 @@
+import Chai from "./Chai";
+
+
+function App() {
+  return (
+
+    <>
+    <Chai/>
+      <h1> chai or code |Basic React App</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01react/src/Chai.jsx
@@ -0,0 +1,9 @@
+function Chai(){
+    return (
+        <>
+        <h2>chai is in function </h2>
+        </>
+    )
+}
+
+export default Chai;
